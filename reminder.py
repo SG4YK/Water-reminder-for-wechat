c493890c0975de0dbc66128c1e50ff107f0fa07b
SG4YK <sg4yk@outlook.com>
2019-05-24 02:29:04 +0800

    Revert "minor fix"
    This reverts commit e308c26ae8981b5e0fa0bbce5dea894682f29f6d.

--- reminder.py
+++ reminder.py
@@ -12,18 +12,25 @@ def checkTime():
 def remind():
     with open('config.json', 'r') as config_file:
         config = load(config_file)
+#       print(imgs)
+#       print(groups)
         for i in range(0,len(config['groups'])):
             names=config['groups'][i]['names']
             messages=config['groups'][i]['messages']
             images=config['groups'][i]['images']
             current_groups=[]
-            for j in range(0,len(names)):
-                current_groups.append(ensure_one(bot.groups().search(names[j])))
+            #search for groups
+            for j in range(0,len(config['groups'][i]['names'])):
+                current_groups.append(ensure_one(bot.groups().search(config['groups'][i]['names'][j])))
+#                current_groups.append(config['groups'][i]['names'][j])
+            #send messages
             for j in range(0,len(current_groups)):
-                for k in range(0,len(messages)):
-                    current_groups[i].send(messages[k])
-                for l in range(0,len(images)):
-                    current_groups[i].send_image(images[k])
+                for k in range(0,len(config['groups'][i]['messages'])):
+#                    print(current_groups[j],' messages:',config['groups'][i]['messages'][k])
+                    current_groups[i].send(config['groups'][i]['messages'][k])
+                for l in range(0,len(config['groups'][i]['images'])):
+#                    print(current_groups[j],' images:',config['groups'][i]['images'][k])
+                    current_groups[i].send_image(config['groups'][i]['images'][k])
         config_file.close()
 
 if __name__ == "__main__":  
