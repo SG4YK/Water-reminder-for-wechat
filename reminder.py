8d12c3de09d1f87b16cb6c8a6a99a933fe38d3a4
SG4YK <sg4yk@outlook.com>
2019-05-24 08:59:42 +0800

    minor fix

--- reminder.py
+++ reminder.py
@@ -13,14 +13,17 @@ def remind():
     with open('config.json', 'r') as config_file:
         config = load(config_file)
         for i in range(0,len(config['groups'])):
+            names=config['groups'][i]['names']
+            messages=config['groups'][i]['messages']
+            images=config['groups'][i]['images']
             current_groups=[]
-            for j in range(0,len(config['groups'][i]['names'])):
-                current_groups.append(ensure_one(bot.groups().search(config['groups'][i]['names'][j])))
+            for j in range(0,len(names)):
+                current_groups.append(ensure_one(bot.groups().search(names[j])))
             for j in range(0,len(current_groups)):
-                for k in range(0,len(config['groups'][i]['messages'])):
-                    current_groups[i].send(config['groups'][i]['messages'][k])
-                for l in range(0,len(config['groups'][i]['images'])):
-                    current_groups[i].send_image(config['groups'][i]['images'][k])
+                for k in range(0,len(messages)):
+                    current_groups[i].send(messages[k])
+                for l in range(0,len(images)):
+                    current_groups[i].send_image(images[k])
         config_file.close()
 
 if __name__ == "__main__":  
